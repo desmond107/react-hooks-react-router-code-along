82931a180e3a80caf0b4600ac399140e73d22d4a
desmond <desmond@desmonds-MacBook-Pro.local>
2022-05-16 16:01:52 +0300

    task complete

--- src/index.js
+++ src/index.js
@@ -1,10 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 
 function Home() {
   return (
     <div>
       <h1>Home!</h1>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h1>This is my about component!</h1>
+    </div>
+  );
+}
+
+function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <form>
+        <div>
+          <input type="text" name="username" placeholder="Username" />
+        </div>
+        <div>
+          <input type="password" name="password" placeholder="Password" />
+        </div>
+        <input type="submit" value="Submit" />
+      </form>
     </div>
   );
 }
